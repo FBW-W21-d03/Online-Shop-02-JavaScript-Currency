3c069a9c08fb699ead3acd5e3cb4a0215be934b9
Vens <v.kolev@alfatrex.com>
2021-12-01 15:01:22 +0100

    Vens lösung fix

--- scripts.js
+++ scripts.js
@@ -20,94 +20,91 @@ function myFunction(){
 
     euro = !euro
     werrung = !werrung
-    if (werrung){ document.getElementById("euroButton").innerHTML = "Euro";
+    
+    if (euro){ document.getElementById("price").innerHTML = (document.getElementById("price").innerHTML / 1.13).toFixed(2);
              }
-    else { document.getElementById("euroButton").innerHTML = "Dollar";
-         }
-    if (euro){ document.getElementById("price").innerHTML = (document.getElementById("price").innerHTML * 1.14).toFixed(2);
-             }
-    else { document.getElementById("price").innerHTML = (document.getElementById("price").innerHTML / 1.14).toFixed(2);
+    else { document.getElementById("price").innerHTML = (document.getElementById("price").innerHTML * 1.13).toFixed(2);
          }
      
     if (werrung){ document.getElementById("werrung").innerHTML = "€";
              }
     else { document.getElementById("werrung").innerHTML = "$";
          }
-    if (euro){ document.getElementById("price1").innerHTML = (document.getElementById("price1").innerHTML * 1.14).toFixed(2);
+    if (euro){ document.getElementById("price1").innerHTML = (document.getElementById("price1").innerHTML / 1.13).toFixed(2);
              }
-    else { document.getElementById("price1").innerHTML = (document.getElementById("price1").innerHTML / 1.14).toFixed(2);
+    else { document.getElementById("price1").innerHTML = (document.getElementById("price1").innerHTML * 1.13).toFixed(2);
          }
      
     if (werrung){ document.getElementById("werrung1").innerHTML = "€";
              }
     else { document.getElementById("werrung1").innerHTML = "$";
          }
-      if (euro){ document.getElementById("price2").innerHTML = (document.getElementById("price2").innerHTML * 1.14).toFixed(2);
+      if (euro){ document.getElementById("price2").innerHTML = (document.getElementById("price2").innerHTML / 1.13).toFixed(2);
              }
-    else { document.getElementById("price2").innerHTML = (document.getElementById("price2").innerHTML / 1.14).toFixed(2);
+    else { document.getElementById("price2").innerHTML = (document.getElementById("price2").innerHTML * 1.13).toFixed(2);
          }
      
     if (werrung){ document.getElementById("werrung2").innerHTML = "€";
              }
     else { document.getElementById("werrung2").innerHTML = "$";
          }
-    if (euro){ document.getElementById("price3").innerHTML = (document.getElementById("price3").innerHTML * 1.14).toFixed(2);
+    if (euro){ document.getElementById("price3").innerHTML = (document.getElementById("price3").innerHTML / 1.13).toFixed(2);
              }
-    else { document.getElementById("price3").innerHTML = (document.getElementById("price3").innerHTML / 1.14).toFixed(2);
+    else { document.getElementById("price3").innerHTML = (document.getElementById("price3").innerHTML * 1.13).toFixed(2);
          }
      
     if (werrung){ document.getElementById("werrung3").innerHTML = "€";
              }
     else { document.getElementById("werrung3").innerHTML = "$";
          }           
-    if (euro){ document.getElementById("price4").innerHTML = (document.getElementById("price4").innerHTML * 1.14).toFixed(2);
+    if (euro){ document.getElementById("price4").innerHTML = (document.getElementById("price4").innerHTML / 1.13).toFixed(2);
              }
-    else { document.getElementById("price4").innerHTML = (document.getElementById("price4").innerHTML / 1.14).toFixed(2);
+    else { document.getElementById("price4").innerHTML = (document.getElementById("price4").innerHTML * 1.13).toFixed(2);
          }
      
     if (werrung){ document.getElementById("werrung4").innerHTML = "€";
              }
     else { document.getElementById("werrung4").innerHTML = "$";
          } 
-    if (euro){ document.getElementById("price5").innerHTML = (document.getElementById("price5").innerHTML * 1.14).toFixed(2);
+    if (euro){ document.getElementById("price5").innerHTML = (document.getElementById("price5").innerHTML / 1.13).toFixed(2);
              }
-    else { document.getElementById("price5").innerHTML = (document.getElementById("price5").innerHTML / 1.14).toFixed(2);
+    else { document.getElementById("price5").innerHTML = (document.getElementById("price5").innerHTML * 1.13).toFixed(2);
          }
      
     if (werrung){ document.getElementById("werrung5").innerHTML = "€";
              }
     else { document.getElementById("werrung5").innerHTML = "$";
          }
-    if (euro){ document.getElementById("price6").innerHTML = (document.getElementById("price6").innerHTML * 1.14).toFixed(2);
+    if (euro){ document.getElementById("price6").innerHTML = (document.getElementById("price6").innerHTML / 1.13).toFixed(2);
              }
-    else { document.getElementById("price6").innerHTML = (document.getElementById("price6").innerHTML / 1.14).toFixed(2);
+    else { document.getElementById("price6").innerHTML = (document.getElementById("price6").innerHTML * 1.13).toFixed(2);
          }
      
     if (werrung){ document.getElementById("werrung6").innerHTML = "€";
              }
     else { document.getElementById("werrung6").innerHTML = "$";
          }
-    if (euro){ document.getElementById("price7").innerHTML = (document.getElementById("price7").innerHTML * 1.14).toFixed(2);
+    if (euro){ document.getElementById("price7").innerHTML = (document.getElementById("price7").innerHTML / 1.13).toFixed(2);
              }
-    else { document.getElementById("price7").innerHTML = (document.getElementById("price7").innerHTML / 1.14).toFixed(2);
+    else { document.getElementById("price7").innerHTML = (document.getElementById("price7").innerHTML * 1.13).toFixed(2);
          }
      
     if (werrung){ document.getElementById("werrung7").innerHTML = "€";
              }
     else { document.getElementById("werrung7").innerHTML = "$";
          }
-    if (euro){ document.getElementById("price8").innerHTML = (document.getElementById("price8").innerHTML * 1.14).toFixed(2);
+    if (euro){ document.getElementById("price8").innerHTML = (document.getElementById("price8").innerHTML / 1.13).toFixed(2);
              }
-    else { document.getElementById("price8").innerHTML = (document.getElementById("price8").innerHTML / 1.14).toFixed(2);
+    else { document.getElementById("price8").innerHTML = (document.getElementById("price8").innerHTML * 1.13).toFixed(2);
          }
      
     if (werrung){ document.getElementById("werrung8").innerHTML = "€";
              }
     else { document.getElementById("werrung8").innerHTML = "$";
          }
-    if (euro){ document.getElementById("price9").innerHTML = (document.getElementById("price9").innerHTML * 1.14).toFixed(2);
+    if (euro){ document.getElementById("price9").innerHTML = (document.getElementById("price9").innerHTML / 1.13).toFixed(2);
              }
-    else { document.getElementById("price9").innerHTML = (document.getElementById("price9").innerHTML / 1.14).toFixed(2);
+    else { document.getElementById("price9").innerHTML = (document.getElementById("price9").innerHTML * 1.13).toFixed(2);
          }
      
     if (werrung){ document.getElementById("werrung9").innerHTML = "€";
