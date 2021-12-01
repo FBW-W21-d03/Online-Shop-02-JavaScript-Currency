4ee84280ac541ae9b2cce96dd0ef0c57b0895957
Dennisbullys <d.rochus@hotmail.de>
2021-12-01 13:41:39 +0100

    Dennis

---
+++ java.js
@@ -0,0 +1,26 @@
+function change() {
+  document.getElementById("wechsel").innerHTML = 1699 * 1.14.toFixed(2)
+}
+
+function change1 () {
+  document.getElementById("wechsel1").innerHTML = 499 * 1.14.toFixed(1);
+}
+
+function change2() {
+  document.getElementById("wechsel2").innerHTML = 1899 * 1.14.toFixed(1);
+}
+function change3() {
+  document.getElementById("wechsel3").innerHTML = 399 * 1.14.toFixed(1);
+}
+function change4() {
+  document.getElementById("wechsel4").innerHTML = 399 * 1.14.toFixed(1);
+}
+function change5() {
+  document.getElementById("wechsel5").innerHTML = 19 * 1.14.toFixed(1);
+}
+function change6() {
+  document.getElementById("wechsel6").innerHTML = 39 * 1.14.toFixed(1)
+}
+function change7() {
+  document.getElementById("wechsel7").innerHTML = 49 * 1.14.toFixed(1)
+}
